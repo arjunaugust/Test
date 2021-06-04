db286c552b232b7692711a4aa064951561ca9d18
arjun <arjun@LAPTOP-3CEN23JN>
2021-06-04 18:39:50 +0530

    Promoiton Service class updated for additinal offer.

--- PromotionsCheckout/Services/PromotionServices.cs
+++ PromotionsCheckout/Services/PromotionServices.cs
@@ -1,4 +1,5 @@
 ﻿using PromotionsCheckout.Entities;
+using PromotionsCheckout.Services.PromotionStrategiesServices;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,9 +14,35 @@ namespace PromotionsCheckout.Services
             ProductOffers productOffers = new ProductOffers();
 
             List<Interfaces.IPromotionStrategy> strategies = new List<Interfaces.IPromotionStrategy>();
+            strategies.Add(new AdditionalOffer());
 
-            
-            throw new NotImplementedException();
+            try
+            {
+                foreach (ProductCheckout item in checkoutProductList)
+                {
+                    if(item.Quantity > 0)
+                    {
+                        foreach (var strategy in strategies)
+                        {
+                            if(strategy.IsExecute(item,promotions))
+                            {
+                                item.InOffer = true;
+                                item.FinalPrice = strategy.CalculcateFinalProductPrice(checkoutProductList);
+                                productOffers.TotalPrice += item.FinalPrice;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                productOffers.CheckoutItems = checkoutProductList;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error during applied offer in Promotion strategy: {ex.Message}");
+            }
+            return productOffers;
         }
     }
 }
