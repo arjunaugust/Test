534581f15ac50c5b503b3d1e790b70cbad8fcfa0
arjun <arjun@LAPTOP-3CEN23JN>
2021-06-04 18:01:04 +0530

    PromotionService Interface created and Method added.

---
+++ PromotionsCheckout/Interfaces/IPromotionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionsCheckout.Interfaces
+{
+    public interface IPromotionService
+    {
+        Entities.ProductOffers ApplyProductPromotion(List<Entities.ProductCheckout> checkoutProductList, List<Entities.Promotions> promotions);
+    }
+}
