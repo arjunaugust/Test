7dc15b11b7e509d5b139c74f9cfed9f1e7949c98
arjun <arjun@LAPTOP-3CEN23JN>
2021-06-04 18:02:30 +0530

    Promotion Service , implement interface IPromotionInterface

---
+++ PromotionsCheckout/Services/PromotionServices.cs
@@ -0,0 +1,15 @@
+﻿using PromotionsCheckout.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionsCheckout.Services
+{
+    public class PromotionServices : Interfaces.IPromotionService
+    {
+        public ProductOffers ApplyProductPromotion(List<ProductCheckout> checkoutProductList, List<Promotions> promotions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
