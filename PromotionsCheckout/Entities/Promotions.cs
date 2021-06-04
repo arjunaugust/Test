91f8999852c29c88364ac4a59f5ddf8d080255d4
arjun <arjun@LAPTOP-3CEN23JN>
2021-06-04 17:42:39 +0530

    Promotion Class created.

---
+++ PromotionsCheckout/Entities/Promotions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionsCheckout.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Promotions
+    {
+
+    }
+}
