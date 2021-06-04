56eac51945c943430a13f9fd27ef418f3bb13df9
arjun <arjun@LAPTOP-3CEN23JN>
2021-06-04 17:54:22 +0530

    Promotions Class Entity udpated.

--- PromotionsCheckout/Entities/Promotions.cs
+++ PromotionsCheckout/Entities/Promotions.cs
@@ -9,6 +9,14 @@ namespace PromotionsCheckout.Entities
     /// </summary>
     public class Promotions
     {
+        public int PromotionId { get; set; }
 
+        public string PromotionType { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double PromotionPrice { get; set; }
     }
 }
