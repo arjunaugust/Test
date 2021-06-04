7dffb2338dfe36d6b7fc4c0cc44aca317fe85f3e
arjun <arjun@LAPTOP-3CEN23JN>
2021-06-04 17:35:28 +0530

    Main Project Created.

---
+++ PromotionsCheckout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionsCheckout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
