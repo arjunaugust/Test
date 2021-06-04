1195bd029d864e09a44cdc2ba32ac2d19651120b
arjun <arjun@LAPTOP-3CEN23JN>
2021-06-04 17:58:56 +0530

    Product offer class created and updated property.

---
+++ PromotionsCheckout/Entities/ProductOffers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionsCheckout.Entities
+{
+    public class ProductOffers
+    {
+        public List<ProductCheckout> CheckoutItems { get; set; }
+
+        public double TotalPrice { get; set; }
+    }
+}
